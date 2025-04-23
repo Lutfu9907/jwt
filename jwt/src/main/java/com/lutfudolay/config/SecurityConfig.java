f0a1720b895c6c09bd20da00fc2d4692dd076b18
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:49:17 +0300

    Update SecurityConfig.java

--- jwt/src/main/java/com/lutfudolay/config/SecurityConfig.java
+++ jwt/src/main/java/com/lutfudolay/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.lutfudolay.jwt.AuthEntryPoint;
 import com.lutfudolay.jwt.JwtAuthenticationFilter;
 
 @Configuration
@@ -17,14 +18,24 @@ import com.lutfudolay.jwt.JwtAuthenticationFilter;
 
 public class SecurityConfig {//Uygulamanın hangi endpoint'lerine kim ulaşabilir, nasıl giriş yapılır, token nasıl kontrol edilir gibi güvenlik politikalarını burada tanımlarız.
 
-	public static final  String AUTHENTICATE = "/authenticate"; //Public erişime açık olacak endpoint’leri sabit olarak tanımlıyorum.
-	public static final  String REGISTER = "/register";
+	public static final String AUTHENTICATE = "/authenticate"; //Public erişime açık olacak endpoint’leri sabit olarak tanımlıyorsun.
+	public static final String REGISTER = "/register";
+	public static final String REFRESH_TOKEN="/refreshToken";
 	
 	@Autowired
-	private AuthenticationProvider authenticationProvider;
+	private  AuthenticationProvider authenticationProvider;
 	
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	@Autowired
+	private AuthEntryPoint authEntryPoint;
+	
+	public static final String[] SWAGGER_PATHS = { // Swagger bileşenlerinin URL yolları bu diziye eklendi.
+			"/swagger-ui/**",
+			"/v3/api-docs/**",
+			"/swagger-ui.html"
+	};
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -34,15 +45,20 @@ public class SecurityConfig {//Uygulamanın hangi endpoint'lerine kim ulaşabili
 		
 		http.csrf().disable() //CSRF (Cross-Site Request Forgery) saldırılarına karşı koruma.
 		.authorizeHttpRequests(request->
-		 request.requestMatchers(AUTHENTICATE,REGISTER)
-		.permitAll() //"/authenticate" ve "/register" adreslerine herkes erişebilir (permitAll()).
+		 request.requestMatchers(AUTHENTICATE,REGISTER,REFRESH_TOKEN).permitAll()//"/authenticate" ve "/register" adreslerine herkes erişebilir (permitAll()).
+		 .requestMatchers(SWAGGER_PATHS).permitAll() //Swagger UI'ye tarayıcıdan erişildiğinde kimlik doğrulama istenmez.
 		.anyRequest()
 		.authenticated()) // Diğer tüm istekler JWT token ile doğrulanmalıdır (authenticated()).
+		.exceptionHandling().authenticationEntryPoint(authEntryPoint).and()
 		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.authenticationProvider(authenticationProvider)
+		
 		//STATELESS = Sunucu tarafında oturum tutulmaz.
 		//Çünkü biz JWT kullanıyoruz. Yani her istek kendi içinde token taşıyor.
 		//Spring, hiçbir kullanıcı bilgisini hafızasında tutmaz.
-		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); 
+		
+		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
 		//jwtAuthenticationFilter filtresi, UsernamePasswordAuthenticationFilter'dan önce çalışacak.
 		//Yani önce token kontrolü yapılır, sonra login kontrolleri devreye girer.
 		
