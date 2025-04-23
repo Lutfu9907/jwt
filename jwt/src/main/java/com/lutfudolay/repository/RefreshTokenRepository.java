89e99c71ca3ebccfdc7a336ed665c7bcf41ccfe7
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:55:45 +0300

    Add files via upload

---
+++ jwt/src/main/java/com/lutfudolay/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.lutfudolay.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lutfudolay.model.RefreshToken;
+
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
+
+	Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
