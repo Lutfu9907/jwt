4eb9db5df26b5c8dac0ee3fc7e4845775bc156c7
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:50:22 +0300

    Update RestAuthControllerImpl.java

--- jwt/src/main/java/com/lutfudolay/controller/impl/RestAuthControllerImpl.java
+++ jwt/src/main/java/com/lutfudolay/controller/impl/RestAuthControllerImpl.java
@@ -8,28 +8,38 @@ import com.lutfudolay.controller.IRestAuthController;
 import com.lutfudolay.dto.DtoUser;
 import com.lutfudolay.jwt.AuthRequest;
 import com.lutfudolay.jwt.AuthResponse;
+import com.lutfudolay.jwt.RefreshTokenRequest;
 import com.lutfudolay.service.IAuthService;
+import com.lutfudolay.service.IRefreshTokenService;
 
 import jakarta.validation.Valid;
 
-public class RestAuthControllerImpl implements IRestAuthController{
+public class RestAuthControllerImpl implements IRestAuthController {
 
 	@Autowired
 	private IAuthService authService;
-	
+
+	@Autowired
+	private IRefreshTokenService refreshTokenService;
+
 	@PostMapping("/register")
 	@Override
 	public DtoUser register(@Valid @RequestBody AuthRequest request) {
-		
+
 		return authService.register(request);
 	}
 
 	@PostMapping("/authenticate")
 	@Override
 	public AuthResponse authenticate(@Valid @RequestBody AuthRequest request) {
-		
+
 		return authService.authenticate(request);
 	}
 
-	
+	@PostMapping("/refreshToken")
+	@Override
+	public AuthResponse refreshToken(@RequestBody RefreshTokenRequest request) {
+
+		return refreshTokenService.refreshToken(request);
+	}
 }
