29a4ce87bba04ff1fd5b0720bb125e7db30f878b
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:54:43 +0300

    Update JwtService.java

--- jwt/src/main/java/com/lutfudolay/jwt/JwtService.java
+++ jwt/src/main/java/com/lutfudolay/jwt/JwtService.java
@@ -2,6 +2,8 @@ package com.lutfudolay.jwt;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,26 +22,38 @@ public class JwtService {
 
 	public String generateToken(UserDetails userDetails) {  //"Bir kullanıcı sisteme login olduğunda, bu kullanıcı için bir JWT token üret."
 		
-		//Map<String, String> claimsMap = new HashMap<>();
-		//claimsMap.put("role","ADMIN");
+		Map<String, Object> claimsMap = new HashMap<>();
+		claimsMap.put("role","ADMIN");
 		
 		return Jwts.builder() // Jwts sınıfının builder metodu kullanıldı
 		.setSubject(userDetails.getUsername()) // Bu token kime ait? Genelde kullanıcı adı (username) konur.
+		.addClaims(claimsMap)
 		.setIssuedAt(new Date()) // Ne zaman üretildi?
 		.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2)) // Ne zaman geçerliliği biter? (şu an + 2 saat)
 		.signWith(getKey(), SignatureAlgorithm.HS256) //Token’ı imzala (Key ve algoritma ile)
 		.compact(); // Token’ı string haline getir ve döndür.  Dönen değer: Bir String token. Bu token kullanıcıya frontend’e gönderilir.
 
 	}
+	
+	public Object getClaimsByKey(String token,String key) {
+		Claims claims = getClaims(token);
+		return claims.get(key);
+		
+	}
+	
+	public Claims getClaims(String token) {
+		Claims claims = Jwts 
+				.parserBuilder()		
+				.setSigningKey(getKey())
+				.build()
+				.parseClaimsJws(token).getBody();
+		
+		return claims;
+	}
 
 	public <T> T exportToken(String token, Function<Claims, T> claimsFunction) { //"Verilen bir JWT token’ı çöz ve içerisindeki bilgileri oku"
 		
-		Claims claims = Jwts 
-		.parserBuilder()		
-		.setSigningKey(getKey())
-		.build()
-		.parseClaimsJws(token).getBody();
-		
+		Claims claims = getClaims(token);
 		return claimsFunction.apply(claims);
 		
 		//Token’ı alır, çözümlemek için aynı Key ile açar. 
