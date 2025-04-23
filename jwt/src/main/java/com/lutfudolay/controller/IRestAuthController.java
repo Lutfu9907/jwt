64b1b2c03ee7582073f13291b6ba18bd7a24d83c
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:50:58 +0300

    Update IRestAuthController.java

--- jwt/src/main/java/com/lutfudolay/controller/IRestAuthController.java
+++ jwt/src/main/java/com/lutfudolay/controller/IRestAuthController.java
@@ -9,4 +9,6 @@ public interface IRestAuthController{
 	public DtoUser register (AuthRequest request);
 	
 	public AuthResponse authenticate(AuthRequest request) ;
+
+	public AuthResponse refreshToken(RefreshTokenRequest request) ;
 }
