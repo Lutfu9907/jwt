33593c0dad94c56cdbb7d86641f0b12405a18006
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-23 16:57:51 +0300

    Update AuthServiceImpl.java

--- jwt/src/main/java/com/lutfudolay/service/impl/AuthServiceImpl.java
+++ jwt/src/main/java/com/lutfudolay/service/impl/AuthServiceImpl.java
@@ -1,6 +1,8 @@
 package com.lutfudolay.service.impl;
 
+import java.sql.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,9 @@ import com.lutfudolay.dto.DtoUser;
 import com.lutfudolay.jwt.AuthRequest;
 import com.lutfudolay.jwt.AuthResponse;
 import com.lutfudolay.jwt.JwtService;
+import com.lutfudolay.model.RefreshToken;
 import com.lutfudolay.model.User;
+import com.lutfudolay.repository.RefreshTokenRepository;
 import com.lutfudolay.repository.UserRepository;
 import com.lutfudolay.service.IAuthService;
 
@@ -33,6 +37,17 @@ public class AuthServiceImpl implements IAuthService {
 	@Autowired
 	private JwtService jwtService;
 	
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+	
+	private RefreshToken createRefreshToken(User user) {
+		RefreshToken refreshToken = new RefreshToken();
+		refreshToken.setRefreshToken(UUID.randomUUID().toString());
+		refreshToken.setExpireDate(new Date(System.currentTimeMillis()+1000*60*60*4));
+		refreshToken.setUser(user);
+		
+		return refreshToken;
+	}
 	
 	@Override
 	public AuthResponse authenticate(AuthRequest request) {
@@ -41,9 +56,12 @@ public class AuthServiceImpl implements IAuthService {
 			authenticationProvider.authenticate(auth);
 			
 			Optional<User> optionalUser = userRepository.findByUsername(request.getUsername());
-			String token = jwtService.generateToken(optionalUser.get());
-		
-			return new AuthResponse(token);
+			String accessToken = jwtService.generateToken(optionalUser.get());
+			
+			RefreshToken refreshToken = createRefreshToken(optionalUser.get());
+			refreshTokenRepository.save(refreshToken);
+			
+			return new AuthResponse(accessToken,refreshToken.getRefreshToken());
 			
 		} catch (Exception e) {
 			System.out.println("kullanıcı adı ya da şifre hatalı");
@@ -63,6 +81,4 @@ public class AuthServiceImpl implements IAuthService {
 		BeanUtils.copyProperties(savedUser, dto);
 		return dto;
 	}
-
-
 }
